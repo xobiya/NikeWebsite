f72399221d3893ff0f1d226f1f4623ac8bdff1c8
xobiya <eshetufeleke21@gmail.com>
2025-05-05 23:16:22 +0300

    data file

---
+++ src/Sections/index.js
@@ -0,0 +1,18 @@
+import Hero from "./Hero";
+import PopularProducts from "./PopularProducts";
+import SpecialOffers from "./SpecialOffers";
+import CustomerReviews from "./CustomerReviews";
+import Subscribe from "./Subscribe";
+import Footer from "./Footer";
+import SuperQuality from "./SuperQuality";
+import Services from "./Services";
+export {
+    Hero,
+    PopularProducts,
+    SpecialOffers,
+    CustomerReviews,
+    Subscribe,
+    Footer,
+    SuperQuality,
+    Services,
+}
