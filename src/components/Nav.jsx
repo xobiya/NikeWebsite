c1190d0e860cfc8e2391f5d37c25876ff612cba0
xobiya <eshetufeleke21@gmail.com>
2025-11-15 19:06:37 +0300

    first  commite

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,21 +1,23 @@
-import headerLogo from '../assets/images/header-logo.svg'
-import hamburger from '../assets/icons/hamburger.svg'
-import {navLinks} from '../Constants'
+import headerLogo from "../assets/images/header-logo.svg";
+import hamburger from "../assets/icons/hamburger.svg";
+import { navLinks } from "../Constants";
+import { Link } from "react-router-dom";
+
 const Nav = () => {
   return (
     <header className='padding-x py-8 absolute z-10 w-full'>
         <nav className='flex justify-between items-center max-container'>
-            <a href="/">
-            <img src={headerLogo} alt="Logo" width={130} height={29}/>
-            </a>
+            <Link to="/">
+              <img src={headerLogo} alt="Logo" width={130} height={29} />
+            </Link>
             <ul className='flex-1 flex justify-center items-center gap-16 max-lg:hidden'>
                 {navLinks.map((link) => (
                     <li key={link.label}>
-                        <a href={link.label} className='font-montserrat leading-normal text-lg text-slate-gray'>
+                        <Link to={link.href} className='font-montserrat leading-normal text-lg text-slate-gray'>
                             {link.label}
-                        </a>
-                        </li>),
-                )}
+                        </Link>
+                    </li>
+                ))}
             </ul>
                 <div className='hidden max-lg:block ' >
                     <img src={hamburger} width={20} height={20} alt="menu" />
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./index.css";
+import App from "./App.jsx";
+import AboutPage from "./pages/AboutPage";
+import ProductsPage from "./pages/ProductsPage";
+import ContactPage from "./pages/ContactPage";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/products" element={<ProductsPage />} />
+        <Route path="/contact" element={<ContactPage />} />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
+);
